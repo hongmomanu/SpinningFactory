4682917e384a7d258fe19cc7737338c758eae223
jack <hongmomanu@163.com>
2015-06-26 10:26:16 +0800

    jack commit

--- app/controller/Login.js
+++ app/controller/Login.js
@@ -118,6 +118,12 @@ Ext.define('SpinningFactory.controller.Login', {
     },
     autoLogin:function(){
 
+        if(localStorage.isfactory){
+
+        }else{
+
+        }
+
         var userinfo=JSON.parse(localStorage.user);
 
         if(userinfo){
